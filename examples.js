fdf91ada8048ca5041a43a824462b2320e67d3fd
QendresaS <qendresaskenderi2000@gmail.com>
2021-04-02 17:41:10 +0200

    hw

---
+++ examples.js
@@ -0,0 +1,89 @@
+//----example1----  
+//Request a user’s name and display the response “Hello, [name]”.
+
+//let name = prompt("Write your name: ");
+//console.log("Hello " + name);
+
+//----example2----
+//Request a user’s year of birth, count his age, 
+//and display the result. Store the current year as a constant. 
+
+//let birthyear = prompt("Write your birth year: ");
+//const CURRENTYEAR = "2021";
+//let age = CURRENTYEAR - birthyear;
+//console.log("You're " + age + " years old");
+
+//----example3----
+//Request a length of a side of a square from a user 
+//and display the perimeter of the square. 
+
+//let length = prompt("Write a length for a squares side:");
+//let perimeter = length*4;
+//console.log("The perimeter of the square is: " + perimeter);
+
+//----example4----
+//Request a radius of a circle and display the area 
+//of such a circle. 
+
+//let radius = prompt("Write a radius for a circle: ");
+//const PI = 3.14159265359;
+//let area = (radius*radius)*PI;
+//console.log("The area of the circle is: "+ area);
+
+//----example5----
+//Request a distance in km between the two cities from a user,
+// and the time they want to cover it within. 
+//Count the speed needed to be on time. 
+
+//let distance = prompt("Write a distance between two cities in kms:");
+//let time = prompt("Write the time you want to cover it within: ");
+//let speed = distance/time;
+//console.log("The speed needed to be on time is: "+ speed + "km/h");
+
+//----example6----
+//Create a currency converter. A user types in USD and the program 
+//converts them to EUR. The exchange rate should be stored as a constant.
+
+//let number = prompt("Write a currency in USD to convert it to EUR: ");
+//const RATE = 0.85;
+//let result = number*RATE;
+//console.log("EUR: " +result);
+
+//----example7----
+//A user states a flash drive memory in GB. The program has to count 
+//how many 820MB files can be stored on this flash drive. 
+
+//let GBMemory = prompt("Number of GB you have:");
+//let convertToMB = parseInt(GBMemory * 1024);
+//const MBValue = 820;
+//let result = parseFloat(convertToMB / MBValue);
+//console.log("You can store " + result + "820MB files on this flash drive"); 
+
+//----example8----
+//A user types in an amount of money in a wallet and the price 
+//of one chocolate bar. The program counts how many chocolate bars 
+//the user can buy, and how much change will he/she have left. 
+
+//let amountMoney = prompt("Write the amount of money you have: ");
+//let chocolatePrice = prompt("Write the price of one chcocolate bar: ");
+//let total = amountMoney/chocolatePrice;
+//console.log("You can buy " +total + " chocolate bars"); 
+//let change = amountMoney%chocolatePrice;
+//console.log("Your change is: " + change);
+
+//----example9----
+//The user types in a sum of bank deposits for 2 months 
+//with a yearly interest rate of 5%. The program counts the sum of interest.
+
+// let sumOfDeposits = parseFloat(prompt('Write the sum of bank deposits for 2 months:'));
+// let interestRate = parseFloat(5/100);
+// let Loan = parseFloat(2/12);
+// let sumOfInterest = interestRate*sumOfDeposits*Loan;
+// console.log('Sum of interest of bank deposits for 2 months with a yearly interest rate of 5% is ' + sumOfInterest);
+
+
+if ("0") 
+{ alert("Hello World"); 
+}
+
+ 
